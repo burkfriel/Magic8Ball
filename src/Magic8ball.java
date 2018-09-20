fb7c21d8e72332323be55e7b1896bbdfa0da4104
Burk Friel <burk.friel.2022@student.lhschools.org>
2018-09-20 14:18:28 -0400

    Made some changes

--- src/Magic8ball.java
+++ src/Magic8ball.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 public class Magic8ball {
 
     public static void main(String[]largs){
+        Scanner keyboard;
 
+        String Question;
+        int number;
 
+        keyboard = new Scanner(System.in);
 
+        System.out.println("This is a magic 8 ball what is your question.");
+        Question = keyboard.nextLine();
+        System.out.println("Now give me a number.");
+        number = keyboard.nextInt();
 
-
-
-
-
+        if (number >= 1 && number <=)
 
 
 
