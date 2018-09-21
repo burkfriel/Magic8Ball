9efc5b7b69b65944c73ef926dc0513e9e92c527f
Burk Friel <burk.friel.2022@student.lhschools.org>
2018-09-21 14:09:55 -0400

    Finished 8 Ball

--- src/Magic8ball.java
+++ src/Magic8ball.java
@@ -15,8 +15,26 @@ public class Magic8ball {
         System.out.println("Now give me a number.");
         number = keyboard.nextInt();
 
-        if (number >= 1 && number <=)
-
+        if (number >= 1 && number <= 10)
+            System.out.println("Yes.");
+        else if (number >= 11 & number <= 20)
+            System.out.println("No.");
+        else if (number >= 21 & number <= 30)
+            System.out.println("No way.");
+        else if (number >= 31 & number <= 40)
+            System.out.println("Absolutely.");
+        else if (number >= 41 & number <= 50)
+            System.out.println("Of course.");
+        else if (number >= 51 & number <= 60)
+            System.out.println("Are you kidding me, no.");
+        else if (number >= 61 & number <= 70)
+            System.out.println("Are you crazy, no.");
+        else if (number >= 71 & number <= 80)
+            System.out.println("Of course not.");
+        else if (number >= 81 & number <= 90)
+            System.out.println("Are you kidding me, yes.");
+        else if (number >= 91 & number <= 100)
+            System.out.println("Are you crazy, yes.");
 
 
     }
