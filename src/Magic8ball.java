8f15b63bf4e958639a69df18a39881073aa532a1
Burk Friel <burk.friel.2022@student.lhschools.org>
2018-10-08 14:16:49 -0400

    changes

--- src/Magic8ball.java
+++ src/Magic8ball.java
@@ -2,40 +2,76 @@ import java.util.Scanner;
 
 public class Magic8ball {
 
-    public static void main(String[]largs){
+    public static void main(String[] largs) {
         Scanner keyboard;
 
         String Question;
         int number;
+        boolean playAgain;
+        playAgain = false;
+        int playAgainn;
+        boolean done;
+        done = false;
+
 
         keyboard = new Scanner(System.in);
 
-        System.out.println("This is a magic 8 ball what is your question.");
-        Question = keyboard.nextLine();
-        System.out.println("Now give me a number.");
-        number = keyboard.nextInt();
-
-        if (number >= 1 && number <= 10)
-            System.out.println("Yes.");
-        else if (number >= 11 & number <= 20)
-            System.out.println("No.");
-        else if (number >= 21 & number <= 30)
-            System.out.println("No way.");
-        else if (number >= 31 & number <= 40)
-            System.out.println("Absolutely.");
-        else if (number >= 41 & number <= 50)
-            System.out.println("Of course.");
-        else if (number >= 51 & number <= 60)
-            System.out.println("Are you kidding me, no.");
-        else if (number >= 61 & number <= 70)
-            System.out.println("Are you crazy, no.");
-        else if (number >= 71 & number <= 80)
-            System.out.println("Of course not.");
-        else if (number >= 81 & number <= 90)
-            System.out.println("Are you kidding me, yes.");
-        else if (number >= 91 & number <= 100)
-            System.out.println("Are you crazy, yes.");
 
 
+        while(!playAgain) {
+
+            System.out.println("This is a magic 8 ball what is your question.");
+            Question = keyboard.nextLine();
+            number = (int) (Math.random() * 100);
+
+            while(!done) {
+
+                if (number >= 1 && number <= 10) {
+                    System.out.println("Yes.");
+                    done = true;
+                } else if (number >= 11 && number <= 20) {
+                    System.out.println("No.");
+                    done = true;
+                } else if (number >= 21 && number <= 30) {
+                    System.out.println("No way.");
+                    done = true;
+                } else if (number >= 31 && number <= 40) {
+                    System.out.println("Absolutely.");
+                    done = true;
+                } else if (number >= 41 && number <= 50) {
+                    System.out.println("Of course.");
+                    done = true;
+                } else if (number >= 51 && number <= 60) {
+                    System.out.println("Are you kidding me, no.");
+                    done = true;
+                } else if (number >= 61 && number <= 70) {
+                    System.out.println("Are you crazy, no.");
+                    done = true;
+                } else if (number >= 71 && number <= 80) {
+                    System.out.println("Of course not.");
+                    done = true;
+                } else if (number >= 81 && number <= 90) {
+                    System.out.println("Are you kidding me, yes.");
+                    done = true;
+                } else if (number >= 91 && number <= 100) {
+                    System.out.println("Are you crazy, yes.");
+                    done = true;
+                }
+            }
+
+
+
+            System.out.println("Would you like to play again 1=Yes 2=No.");
+            playAgainn = keyboard.nextInt();
+            keyboard.nextLine();
+
+            if (playAgainn == 2) {
+                System.out.println("Thank you for playing my magic 8 ball.");
+                playAgain = true;
+            }
+            if (playAgainn == 1) {
+                playAgain = false;
+            }
+        }
     }
 }
